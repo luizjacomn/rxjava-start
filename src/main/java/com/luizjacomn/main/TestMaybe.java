a2f6457d64ac6bd4bfdd7b16e647a91d0bc78408
Luiz Jacó <luizjacomn@gmail.com>
2019-11-25 11:32:30 -0300

    Refatorando classe TestMaybe

--- src/main/java/com/luizjacomn/main/TestMaybe.java
+++ src/main/java/com/luizjacomn/main/TestMaybe.java
@@ -3,6 +3,8 @@ package com.luizjacomn.main;
 import java.math.BigDecimal;
 
 import io.reactivex.Maybe;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 
 /**
  * TestMaybe
@@ -11,8 +13,14 @@ public class TestMaybe {
 
     public static void main(String[] args) {
         Maybe<BigDecimal> maybe = Maybe.just(BigDecimal.valueOf(50));
+        Maybe<BigDecimal> maybeEmpty = Maybe.empty();
 
-        maybe.subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("Finalizado..."));
+        Consumer<BigDecimal> onSuccess = System.out::println;
+        Consumer<Throwable> onError = Throwable::printStackTrace;
+        Action onComplete = () -> System.out.println("Finalizado...");
+
+        maybe.subscribe(onSuccess, onError, onComplete);
+        maybeEmpty.subscribe(onSuccess, onError, onComplete);
     }
 
 }
